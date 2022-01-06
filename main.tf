23fdce074e1b8382360ec1b91dd2949576679145
Yue Yuan <y.b.yuan@accenture.com>
2022-01-06 17:59:33 +0100

    new

--- main.tf
+++ main.tf
@@ -15,18 +15,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-05d34d340fb1d89e5"
+  ami           = "ami-099ccc441b2ef41ec"
   instance_type = "t2.micro"
 
 
-  tags = {
-    git_commit           = "cd8ca729f30f19e4923ee9bca0c634217c0cf20e"
-    git_file             = "main.tf"
-    git_last_modified_at = "2022-01-06 14:27:05"
-    git_last_modified_by = "yuanyue910919@gmail.com"
-    git_modifiers        = "yuanyue910919"
-    git_org              = "coco348"
-    git_repo             = "yor-123"
-    yor_trace            = "cdd0f6f1-5e38-4083-95d5-c2b4233a31b7"
-  }
+
 }
